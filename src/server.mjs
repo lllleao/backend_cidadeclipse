4fa29711e2aada759ea453915159378c34755bbb
leao <lucasleaolima@gmail.com>
2024-09-16 17:51:05 -0300

    teste com a origem direta

--- src/server.mjs
+++ src/server.mjs
@@ -32,13 +32,7 @@ app.use(helmet.contentSecurityPolicy({
 const allowedOrigins = process.env.ALLOWED_ORIGINS
 
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
+    origin: 'https://cidadeclipse.com/',
     methods: 'GET,POST',
     allowedHeaders: 'Content-Type, CSRF-Token'
 }))
